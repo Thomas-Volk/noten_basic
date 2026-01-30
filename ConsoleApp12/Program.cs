05f2fa2a4cfc4f3d8e69887fa20b8062bb727c6c
Thomas Volk <Thomasvolk@hotmail.de>
2026-01-30 12:09:47 +0100

    erste Version

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Note eingeben 1-6");
+            string zahl = Console.ReadLine();
+
+            if (zahl == "1")
+            {
+                Console.WriteLine("sehr gut)");
+            }
+            if (zahl == "2")
+            {
+                Console.WriteLine("gut)");
+            }
+        }
+    }
+}
